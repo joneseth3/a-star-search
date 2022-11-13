b6b6d907a4c51d3e44231df6a050a02fc8f0d8d2
Seth Jones <111699173+joneseth1@users.noreply.github.com>
2022-11-13 07:23:40 -0500

    Added Node.java

---
+++ java/Node.java
@@ -0,0 +1,72 @@
+package aStar;
+
+import java.lang.*;
+
+
+public class Node {
+    private int row, col, f, g, h, type;
+    private Node parent;
+       
+    public Node(int r, int c, int t){
+    row = r;
+    col = c;
+    type = t;
+    parent = null;
+    //type 0 is traverseable, 1 is not
+    }
+    //mutator methods to set values
+    public void setF(){
+    f = g + h;
+    }
+    public void setG(int value){
+    g = value;
+    }
+    public void setH(int value){
+    h = value;
+    }
+    public void setParent(Node n){
+    parent = n;
+    }
+    //accessor methods to get values
+    public int getF(){
+    return f;
+    }
+    public int getG(){
+    return g;
+    }
+    public int getH(){
+    return h;
+    }
+    public Node getParent(){
+    return parent;
+    }
+    public int getRow(){
+    return row;
+    }
+    public int getCol(){
+    return col;
+    }
+    public boolean equals(Object in){
+    //typecast to Node
+    Node n = (Node) in;
+    return row == n.getRow() && col == n.getCol();
+    }
+       
+
+
+    public String toString()
+    {
+    return "[" + row + "," + col + "]";
+    }
+
+    public int getType()
+    {
+        return type; 
+    }
+
+    public void setType(int value)
+    {
+        type = value;
+    }
+
+}
